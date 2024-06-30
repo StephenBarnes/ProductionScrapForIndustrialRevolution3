f713e842f3a9daba0e300bec2a5b6335cc2632ee
Stephen Barnes <stephen6174@gmail.com>
2024-06-30 15:21:32 +0200

    Move scrap processing recipes to data-updates, bump version, change AAI dependency to hidden

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,2 +1,1 @@
 require("code.tweaks.recipes-produce-scrap")
-require("code.tweaks.scrap-processing-recipes")
--- data-updates.lua
+++ data-updates.lua
@@ -1,2 +1,4 @@
+require("code.tweaks.scrap-processing-recipes")
+
 -- This is in data-updates, not data-final-fixes, so that eg Extended Descriptions mod will see the updated stack sizes.
 require("code.tweaks.change-stack-sizes")
